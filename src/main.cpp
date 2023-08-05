f38b8af8ef4fbeb55ef74c37b3635991d7e5a3af
Clement DESAVIS <clement.desavis@outlook.com>
2023-08-05 23:12:29 +0200

    Project Setup

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+#include <cmrc/cmrc.hpp>
+
+CMRC_DECLARE(TheForest);
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    ImGui::SFML::Init(window);
+
+
+    int fps = 60;
+    window.setFramerateLimit(fps);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+
+        if (ImGui::SliderInt("Max Framerate :", &fps, 1, 144))
+        {
+            window.setFramerateLimit(fps);
+        }
+        ImGui::End();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
+}
